4ddc7a4b6b2a9432c43f2853bb236e84760ee361
anton.sorensen@gmail.com <anton.sorensen@gmail.com@fc4ae7c4-fc6d-f8ea-fc5e-9b86253c9dfb>
2011-05-15 20:25:40 +0000

    Added support for writing tech trees to the buildUnit

--- trunk/bot/src/StarcraftBot/BuildUnitAnswer.java
+++ trunk/bot/src/StarcraftBot/BuildUnitAnswer.java
@@ -27,7 +27,29 @@ public class BuildUnitAnswer extends Answer {
 		}
 		String name = actor.name;
 		String buildsAt = actor.buildsAt;
-		String answerString = GrammarEngine.nounPlural(name)+" are "+GrammarEngine.verbPastTense(action)+" at the "+buildsAt;
+		String answerString = String.format("%s are %s at the %s.", 
+				GrammarEngine.nounPlural(name), 
+				GrammarEngine.verbPastTense(action), 
+				buildsAt);
+		
+		//check for techTree. If there is one, add additional text.
+		if (!actor.techTreeList.isEmpty()){
+			answerString += "You will also need ";
+			if(actor.techTreeList.size() > 2){
+				for (int i = 0; i < actor.techTreeList.size()-1; i++) {
+					String item = actor.techTreeList.get(i);
+					answerString += String.format("%s, ", GrammarEngine.nounIndefinite(item));
+				}
+				String lastItem = actor.techTreeList.get(actor.techTreeList.size());
+				answerString += String.format("and %s",GrammarEngine.nounIndefinite(lastItem));
+			}
+			else if(actor.techTreeList.size()==1){
+				String lastItem = actor.techTreeList.get(actor.techTreeList.size());
+				answerString += String.format("%s",GrammarEngine.nounIndefinite(lastItem));
+			}
+				
+		}
+			
 		return answerString;
 	}
 
