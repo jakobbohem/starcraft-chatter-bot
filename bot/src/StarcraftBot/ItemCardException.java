075c3ff57eff1ee1ceec708b72532dda7804e122
jakob.bohem@gmail.com <jakob.bohem@gmail.com@fc4ae7c4-fc6d-f8ea-fc5e-9b86253c9dfb>
2011-05-15 09:48:27 +0000

    Improved on database accessor methods. refactored the ItemCardExceptions so that there is just 1. privatised some lower level mehtods

--- bot/src/StarcraftBot/ItemCardException.java
+++ bot/src/StarcraftBot/ItemCardException.java
@@ -1,6 +1,9 @@
 package StarcraftBot;
 
 public class ItemCardException extends Exception {
+   public ItemCardException(String message){
+       super(message);   }
+
 	public ItemCardException(String msg, ItemCard card){
 		System.err.print(msg);
 	}
