969ab4a670a974d779733b22d575168b82213b74
jakob.bohem@gmail.com <jakob.bohem@gmail.com@fc4ae7c4-fc6d-f8ea-fc5e-9b86253c9dfb>
2011-05-16 11:50:14 +0000

    small changes. lots of catches

--- trunk/bot/src/StarcraftBot/Bot.java
+++ trunk/bot/src/StarcraftBot/Bot.java
@@ -3,7 +3,11 @@
  * and open the template in the editor.
  */
 package StarcraftBot;
+
 import java.util.*;
+import StarcraftBot.DatabaseAccessor.DatabaseException; // for database read/write issues
+import StarcraftBot.Interpreter.SparseSpecException; // for lack of information in input issues
+import StarcraftBot.Tagger.ExecutionOrderException; // general Exception for bad coding issues (CATCH INSIDE?)
 
 /**
  * Testing harmless code submit through Netbeans SVN plugin
@@ -15,55 +19,66 @@ public class Bot {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+
         // Run basic database implementation test:
         DatabaseAccessor dba = new DatabaseAccessor();
         DbTests.test2(dba);
         DbTests.testAdders(dba);
-        
+
         // Run program
         int exitcode = 0;
         Scanner scan = new Scanner(System.in);
-        
-        while(exitcode != 1)
-        {
-            System.out.println("Enter a phrase for tagging: \n(1 to exit)");
-            System.out.print("User: ");
-            String inp = scan.nextLine();
-            
-            // get the exit code, if any
-            try
-            {
-                exitcode = Integer.parseInt(inp);
-            }
-            catch(NumberFormatException e){}
+        try {
+            while (exitcode != 1) {
+                System.out.println("Enter a phrase for tagging: \n(1 to exit)");
+                System.out.print("User: ");
+                String inp = scan.nextLine();
+
+                // get the exit code, if any
+                try {
+                    exitcode = Integer.parseInt(inp);
+                } catch (NumberFormatException e) {
+                }
                 // no number was in the exit string - that's fine for now                
-                
-            // do the parsing and interpretation:
-            try{
-                Tagger tagger = new Tagger();
-                String[] inputs = tagger.chopString(inp); // also sets the lastUsedString property
-                // equal to getLastChunks
-                
-                String[] tags = tagger.getTagsFromDatabase();
-                Tools.printArray(tagger.getLastChunks());
-                Tools.printArray(tags);
-                
-                // do some interpretation:
-                Interpreter Jeeves = new Interpreter();
-                Query q = Jeeves.interpretTags(tags, inputs);
-                // String answer = Jeeves.getReply(q); // old file read method
-                String answer = Jeeves.getReply(q, dba);
-                System.out.println(Jeeves.name+": "+answer);
-            }
-            catch(Exception e){
-                System.out.println("caught exception in main.");
-                System.out.println(e);
-                e.printStackTrace();
-            
-            }
-            // now get the labels and print them!
-        } // end of while
-        
+
+                // do the parsing and interpretation:
+                try {
+                    Tagger tagger = new Tagger();
+                    String[] inputs = tagger.chopString(inp); // also sets the lastUsedString property
+                    // equal to getLastChunks
+
+                    String[] tags = tagger.getTagsFromDatabase();
+                    Tools.printArray(tagger.getLastChunks());
+                    Tools.printArray(tags);
+
+                    // do some interpretation:
+                    Interpreter Jeeves = new Interpreter();
+                    Query q = Jeeves.interpretTags(tags, inputs);
+                    // String answer = Jeeves.getReply(q); // old file read method
+                    String answer = Jeeves.getReply(q, dba);
+                    System.out.println(Jeeves.name + ": " + answer);
+                } catch (DatabaseException dbe) {
+                    System.out.println("DEBUG: Exception in main!");
+                    System.out.println(dbe);
+                    //e.printStackTrace();
+
+                } catch (SparseSpecException e) {
+                    System.out.println("DEBUG: Exception in main!");
+                    System.out.println(e);
+                    //e.printStackTrace();
+
+                } catch (ExecutionOrderException e) {
+                    System.out.println("DEBUG: Exception in main!");
+                    System.out.println(e);
+                    //e.printStackTrace();
+
+                }
+                // now get the labels and print them!
+            } // end of while
+
+        }// end of BIG try
+        catch (java.lang.Exception e) {
+            System.err.println("RUNTIME ERROR!");
+        }
     }
 }
