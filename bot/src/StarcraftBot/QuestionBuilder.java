d240b6c45510435f90930ab45d07b2d90852d8d6
jakob.bohem@gmail.com <jakob.bohem@gmail.com@fc4ae7c4-fc6d-f8ea-fc5e-9b86253c9dfb>
2011-05-17 13:37:55 +0000

    updated default qid-code

--- bot/src/StarcraftBot/QuestionBuilder.java
+++ bot/src/StarcraftBot/QuestionBuilder.java
@@ -4,20 +4,34 @@
  */
 package StarcraftBot;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author antonsorensen
  */
 public class QuestionBuilder {
+    
+    long defaultQID;
+    
     private DatabaseAccessor dba;
-    public QuestionBuilder(DatabaseAccessor dba){
+    public QuestionBuilder(DatabaseAccessor dba) throws SQLite.Exception{
         this.dba = dba;
+        defaultQID = dba.getDefaultQID();
     }
     
-    public int getQID(Query q) throws Exception{
-        String[] md = q.buildSearchPhrase();
-        return dba.getQid(md);
+    public long getQID(Query q) {
+        try {
+            String[] md = q.buildSearchPhrase();
+            return dba.getQid(md);
+        }
+        //This class could possibly be used to define what keywords to search for to narrow down the search in case of multiple answers.
+        catch (Exception ex) {
+            return defaultQID;
+        }
     }
+    
     
     //This class could possibly be used to define what keywords to search for to narrow down the search in case of multiple answers.
 }
